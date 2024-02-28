09fc4fb9675b3bfdb1e347bc9a4ef198dc5e7a98
Carlos Pulgar <carlos911.cp@gmail.com>
2024-02-28 20:06:32 -0300

    parcial finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Card from './component/Card'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     
       <div className='App'>
-        <h1>Carga de estudiantes</h1>
-        <form ></form>
-      <Card/>
+        <h1>Auto preferido</h1>
+       
+      <Card />
       </div>
 
       
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Card = () => {
-  return (
-    <div>
-      hola, soy una tarjeta
+
+
+  const [cliente, setCliente] = useState(
+    {
+      nombre:"",
+      auto:""
+    }
+  )
+
+  const [show, setShow] = useState(false)
+  const [err, setErr] = useState(false)
+
+  const manejador = (e) => {
+    e.preventDefault()
+    if(/^\S.{2,}$/.test(cliente.nombre)){
+        setShow(true)
+        setErr(false)
+    }else{
+      setErr(true)
+    }
+   
+    }
+
+  return (   
+     <div >
+        {!show && <form  onSubmit={manejador} style={{
+      display:"flex",
+      flexDirection:"column",
+     }}  >
+          <label >Nombre Cliente</label>
+          <input type="text" onChange={({target}) => setCliente({...cliente, nombre:target.value }) } />
+          <label> Auto preferido</label>
+          <input type="text" onChange={({target}) => setCliente({...cliente, auto:target.value }) } />
+          <button style={{margin:20}} >ENVIAR</button>
+        </form>}
+        {show && <h4>{cliente.nombre},  tu auto preferido es: {cliente.auto}</h4>}
+        {err && <p style={{color:"red"}}>Ingrese la informacion necesaria</p>}
     </div>
+    
   )
 }
 
